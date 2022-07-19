cad02802e8a8c072584798948f6728186407d0d4
lukablaskovic <59925899+lukablaskovic@users.noreply.github.com>
2022-07-19 11:16:48 +0200

    milvus connection, bug fixes
    - upgraded python version to 3.8.10
    - fixed pyarrow bug
    - declared milvus connection function
    - created indexes

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import cv2
 from towhee._types.image import Image
+from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection, utility
+
 #Read data
 df = pd.read_csv('D:/data/reverse_image_search.csv')
-df.head()
+print(df.head())
 #Data preparation
 id_img = df.set_index('id')['path'].to_dict()
 label_ids = {}
 for label in set(df['label']):
     label_ids[label] = list(df[df['label']==label].id)
-
+#Helper functions
 def read_images(results):
     imgs = []
     for re in results:
@@ -20,3 +22,26 @@ def read_images(results):
 def ground_truth(path):
     label = path.split('/')[-2]
     return label_ids[label]
+
+#Milvus connection
+connections.connect(host='127.0.0.1', port='19530')
+
+def create_milvus_collection(collection_name, dim):
+    if utility.has_collection(collection_name):
+        utility.drop_collection(collection_name)
+    
+    fields = [
+    FieldSchema(name='id', dtype=DataType.INT64, descrition='ids', is_primary=True, auto_id=False),
+    FieldSchema(name='embedding', dtype=DataType.FLOAT_VECTOR, descrition='embedding vectors', dim=dim)
+    ]
+    schema = CollectionSchema(fields=fields, description='reverse image search')
+    collection = Collection(name=collection_name, schema=schema)
+
+    # create IVF_FLAT index for collection.
+    index_params = {
+        'metric_type':'L2',
+        'index_type':"IVF_FLAT",
+        'params':{"nlist":2048}
+    }
+    collection.create_index(field_name="embedding", index_params=index_params)
+    return collection
