faa2daf5e45a31bb728fb0a736c403dc0abd71ed
lukablaskovic <59925899+lukablaskovic@users.noreply.github.com>
2022-07-19 11:32:02 +0200

    code split into 3 files

---
+++ data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import cv2
+from towhee._types.image import Image
+
+#Read data
+df = pd.read_csv('D:/data/reverse_image_search.csv')
+print(df.head())
+#Data preparation
+id_img = df.set_index('id')['path'].to_dict()
+label_ids = {}
+for label in set(df['label']):
+    label_ids[label] = list(df[df['label']==label].id)
+#Helper functions
+def read_images(results):
+    imgs = []
+    for re in results:
+        path = id_img[re.id]
+        imgs.append(Image(cv2.imread(path), 'BGR'))
+    return imgs
+
+def ground_truth(path):
+    label = path.split('/')[-2]
+    return label_ids[label]
---
+++ db.py
@@ -0,0 +1,24 @@
+from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection, utility
+
+#Milvus connection
+connections.connect(host='127.0.0.1', port='19530')
+
+def create_milvus_collection(collection_name, dim):
+    if utility.has_collection(collection_name):
+        utility.drop_collection(collection_name)
+    
+    fields = [
+    FieldSchema(name='id', dtype=DataType.INT64, descrition='ids', is_primary=True, auto_id=False),
+    FieldSchema(name='embedding', dtype=DataType.FLOAT_VECTOR, descrition='embedding vectors', dim=dim)
+    ]
+    schema = CollectionSchema(fields=fields, description='reverse image search')
+    collection = Collection(name=collection_name, schema=schema)
+
+    # create IVF_FLAT index for collection.
+    index_params = {
+        'metric_type':'L2',
+        'index_type':"IVF_FLAT",
+        'params':{"nlist":2048}
+    }
+    collection.create_index(field_name="embedding", index_params=index_params)
+    return collection
