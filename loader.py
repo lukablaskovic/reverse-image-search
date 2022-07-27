66c6e8e1c5aad7d35e8a2825ff8df5bccda1b9cf
lukablaskovic <lblaskovi@unipu.hr>
2022-07-28 00:35:56 +0200

    added comments for towhee functions

--- loader.py
+++ loader.py
@@ -6,9 +6,13 @@ collection = create_milvus_collection('reverse_image_search', 2048)
 
 dc = (
     towhee.read_csv(source)
+      #for each row from the data, convert the data type of the column id from str to int
       .runas_op['id', 'id'](func=lambda x: int(x))
+      #for each row from the data, read and decode the image at path and put the pixel data into column img
       .image_decode['path', 'img']()
+      #extract embedding feature with image_embedding.timm, an operator from the Towhee hub
       .image_embedding.timm['img', 'vec'](model_name='resnet50')
+      #insert image embedding features in to Milvus
       .to_milvus['id', 'vec'](collection=collection, batch=100)
 )
 
